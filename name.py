876c80a5203ee1ef505fc5bb76050957ad835f33
AAP <alexaaron120@gmail.com>
2024-08-18 15:00:34 -0700

    a

--- name.py
+++ name.py
@@ -13,7 +13,8 @@ TEST = '_test.txt'
 
 NODELABEL = '_node_labels.txt'
 NODEATTR = '_node_attributes.txt'
-DATASETS = ["MOLT-4", "Mutagenicity", "SW-620", "NCI-H23", "PC-3", "MCF-7", "NCI109", "AIDS", "DD", "NCI1", "SF-295", "OVCAR-8", "P388", "PROTEINS", "SN12C", "UACC257"]
+DATASETS =  ["SN12C"]
+# ["MOLT-4", "Mutagenicity", "SW-620", "NCI-H23", "PC-3", "MCF-7", "NCI109", "AIDS", "DD", "NCI1", "SF-295", "OVCAR-8", "P388", "PROTEINS", "SN12C", "UACC257"]
 
 group1 = ["MOLT-4", "Mutagenicity", "SW-620"]
 group2 =  ["PC-3", "MCF-7", "NCI109", "AIDS"]
